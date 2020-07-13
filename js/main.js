cc27f899db294888e5e64a31e501e6b9077e254c
Ivan-developer <66819847+Ivan-developer@users.noreply.github.com>
2020-07-13 14:28:11 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(function(){
-
+    // Подключаем slick slider
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+                arrows: false
+            }
+          }
+        ]
+      });
 });
